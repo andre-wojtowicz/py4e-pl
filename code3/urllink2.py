c1634fc41ce2dde6e3818d6acab6c360b5274c1f
Andrzej Wójtowicz <andre@amu.edu.pl>
2021-01-19 15:04:20 +0100

    Minor

--- code3/urllink2.py
+++ code3/urllink2.py
@@ -13,7 +13,7 @@ ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 
-url = input('Enter - ')
+url = input('Podaj link - ')
 html = urlopen(url, context=ctx).read()
 soup = BeautifulSoup(html, "html.parser")
 
