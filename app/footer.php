9554bc674761484379cfd07884394db844c5cf45
Andrzej Wójtowicz <andre-wojtowicz@users.noreply.github.com>
2024-09-25 19:53:41 +0200

    Update footer.php

--- app/footer.php
+++ app/footer.php
@@ -3,6 +3,9 @@
 $foot = '
 <p style="font-size: 75%; margin-top: 5em;">
 Copyright Creative Commons Uznanie autorstwa 3.0 - Charles R. Severance
+</p>
+<p style="font-size: 90%;">
+Strona utrzymywana przez <a href="https://wmi.amu.edu.pl">Wydział Matematyki i Informatyki Uniwersytetu im. Adama Mickiewicza w Poznaniu</a>.
 </p>';
 
 $OUTPUT->setAppFooter($foot);
